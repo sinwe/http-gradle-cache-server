4284cc8c6e4ae2efcdba3a13925bfcf92d3bd0c3
Winarto Zhao <winarto@gmail.com>
2018-05-28 02:43:43 +0800

    Fixing test

--- src/test/java/com/github/sinwe/gradle/caching/httpcacheserver/HttpCacheServerApplicationTests.java
+++ src/test/java/com/github/sinwe/gradle/caching/httpcacheserver/HttpCacheServerApplicationTests.java
@@ -3,7 +3,13 @@ package com.github.sinwe.gradle.caching.httpcacheserver;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,4 +19,11 @@ public class HttpCacheServerApplicationTests {
     public void contextLoads() {
     }
 
+    @TestConfiguration
+    public static class MapConfig {
+        @Bean
+        public ConcurrentMap<String, byte[]> map() {
+            return new ConcurrentHashMap<>();
+        }
+    }
 }
