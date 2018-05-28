ee8fc17ce499aaaf5bdf543ed0b5f71d36f54916
Winarto Zhao <winarto@gmail.com>
2018-05-28 22:15:49 +0800

    Refactored

--- src/test/java/com/github/sinwe/gradle/caching/httpcacheserver/HttpCacheServerApplicationTests.java
+++ src/test/java/com/github/sinwe/gradle/caching/httpcacheserver/HttpCacheServerApplicationTests.java
@@ -5,18 +5,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.util.StringUtils;
 import reactor.core.publisher.Flux;
 
-import java.util.Arrays;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
