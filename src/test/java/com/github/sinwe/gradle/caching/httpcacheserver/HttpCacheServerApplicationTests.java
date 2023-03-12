a3ed5234ed65e22174ed32237b04ed62b832070d
Winarto Zhao <winarto@Winartos-Mac-mini.local>
2023-03-12 23:43:54 +0800

    - Removed bintray - Updated gradle to version 8.0.2 - Updated Java to 17

--- src/test/java/com/github/sinwe/gradle/caching/httpcacheserver/HttpCacheServerApplicationTests.java
+++ src/test/java/com/github/sinwe/gradle/caching/httpcacheserver/HttpCacheServerApplicationTests.java
@@ -1,11 +1,11 @@
 package com.github.sinwe.gradle.caching.httpcacheserver;
 
 import org.jetbrains.annotations.NotNull;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 
@@ -13,7 +13,7 @@ import java.util.UUID;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 public class HttpCacheServerApplicationTests {
 
